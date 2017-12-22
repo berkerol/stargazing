85274d7541afdfa21d914fdcbed15f08d833303a
Berk Erol <berk.erol@boun.edu.tr>
2017-12-23 02:47:51 +0300

    standardization

--- animation.js
+++ animation.js
@@ -3,17 +3,29 @@ let ctx = canvas.getContext("2d");
 let background = document.getElementById("background");
 resizeHandler();
 
-let target = {
-  x: canvas.width / 2,
-  y: canvas.height / 2
-};
-let points = [];
-
 let gap = 25;
 let connections = 8;
 
-let radius = Math.random() * 4 + 2;
-let tweenDuration = Math.random() + 1;
+let mouse = {
+  x: canvas.width / 2,
+  y: canvas.height / 2
+};
+
+let point = {
+  highestDuration: 2,
+  highestRadius: 4,
+  highestX: 50,
+  highestY: 50,
+  lowestDuration: 1,
+  lowestRadius: 2,
+  lowestX: -50,
+  lowestY: -50,
+  color: [255, 215, 0],
+  lineWidth: 2,
+  randomColors: false
+};
+
+let points = [];
 
 for (let x = 0; x < canvas.width; x += canvas.width / gap) {
   for (let y = 0; y < canvas.height; y += canvas.height / gap) {
@@ -24,9 +36,9 @@ for (let x = 0; x < canvas.width; x += canvas.width / gap) {
       originX: px,
       y: py,
       originY: py,
-      red: Math.floor(Math.random() * 255),
-      green: Math.floor(Math.random() * 255),
-      blue: Math.floor(Math.random() * 255)
+      r: point.color[0],
+      g: point.color[1],
+      b: point.color[2]
     });
   }
 }
@@ -34,21 +46,14 @@ for (let p1 of points) {
   let closest = [];
   for (let p2 of points) {
     if (p1 !== p2) {
-      let placed = false;
       for (let i = 0; i < connections; i++) {
-        if (!placed) {
-          if (closest[i] === undefined) {
-            closest[i] = p2;
-            placed = true;
-          }
+        if (closest[i] === undefined) {
+          closest[i] = p2;
+          break;
         }
-      }
-      for (let i = 0; i < connections; i++) {
-        if (!placed) {
-          if (getDistance(p1, p2) < getDistance(p1, closest[i])) {
-            closest[i] = p2;
-            placed = true;
-          }
+        if (getDistance(p1, p2) < getDistance(p1, closest[i])) {
+          closest[i] = p2;
+          break;
         }
       }
     }
@@ -56,88 +61,104 @@ for (let p1 of points) {
   p1.closest = closest;
   p1.circle = {
     position: p1,
-    radius,
-    red: Math.floor(Math.random() * 255),
-    green: Math.floor(Math.random() * 255),
-    blue: Math.floor(Math.random() * 255)
+    radius: point.lowestRadius + Math.random() * (point.highestRadius - point.lowestRadius)
   };
 }
-animate();
+draw();
 for (let p of points) {
   shiftPoint(p);
 }
+document.addEventListener("mousedown", mouseDownHandler);
 document.addEventListener("mousemove", mouseMoveHandler);
 window.addEventListener("resize", resizeHandler);
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let p of points) {
+    if (p.alpha > 0) {
+      for (let c of p.closest) {
+        drawLine(p, c);
+      }
+    }
+  }
+  for (let p of points) {
+    if (p.circle.alpha > 0) {
+      drawCircle(p);
+    }
+  }
+  processPoints();
+  requestAnimationFrame(draw);
+}
+
+function drawLine(p, c) {
+  ctx.beginPath();
+  ctx.moveTo(p.x, p.y);
+  ctx.lineTo(c.x, c.y);
+  ctx.lineWidth = point.lineWidth;
+  ctx.strokeStyle = "rgba(" + c.r + "," + c.g + "," + c.b + "," + p.alpha + ")";
+  ctx.stroke();
+  ctx.closePath();
+}
+
+function drawCircle(p) {
+  ctx.beginPath();
+  ctx.arc(p.circle.position.x, p.circle.position.y, p.circle.radius, 0, 2 * Math.PI);
+  ctx.fillStyle = "rgba(" + p.r + "," + p.g + "," + p.b + "," + p.circle.alpha + ")";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function processPoints() {
+  for (let p of points) {
+    if (Math.abs(getDistance(mouse, p)) < 4000) {
+      p.alpha = 0.3;
+      p.circle.alpha = 0.6;
+    } else if (Math.abs(getDistance(mouse, p)) < 20000) {
+      p.alpha = 0.1;
+      p.circle.alpha = 0.3;
+    } else if (Math.abs(getDistance(mouse, p)) < 40000) {
+      p.alpha = 0.02;
+      p.circle.alpha = 0.1;
+    } else {
+      p.alpha = 0;
+      p.circle.alpha = 0;
+    }
+  }
+}
+
+function shiftPoint(p) {
+  if (point.randomColors) {
+    assignRandomColor(p);
+  }
+  TweenLite.to(p, point.lowestDuration + Math.random() * (point.highestDuration - point.lowestDuration), {
+    x: p.originX + point.lowestX + Math.random() * (point.highestX - point.lowestX),
+    y: p.originY + point.lowestY + Math.random() * (point.highestY - point.lowestY),
+    ease: Sine.easeInOut,
+    onComplete: function() {
+      shiftPoint(p);
+    }
+  });
+}
 
 function getDistance(p1, p2) {
   return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
 }
 
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function assignRandomColor(p) {
+  p.r = Math.floor(Math.random() * 255);
+  p.g = Math.floor(Math.random() * 255);
+  p.b = Math.floor(Math.random() * 255);
+}
+
+function mouseDownHandler(e) {
   for (let p of points) {
-    drawPoint(p);
-    drawLines(p);
-    drawCircle(p.circle);
+    assignRandomColor(p);
   }
-  requestAnimationFrame(animate);
-}
-
-function drawPoint(point) {
-  if (Math.abs(getDistance(target, point)) < 4000) {
-    point.active = 0.3;
-    point.circle.active = 0.6;
-  } else if (Math.abs(getDistance(target, point)) < 20000) {
-    point.active = 0.1;
-    point.circle.active = 0.3;
-  } else if (Math.abs(getDistance(target, point)) < 40000) {
-    point.active = 0.02;
-    point.circle.active = 0.1;
-  } else {
-    point.active = 0;
-    point.circle.active = 0;
-  }
-}
-
-function drawLines(point) {
-  if (point.active) {
-    for (let p of point.closest) {
-      ctx.beginPath();
-      ctx.moveTo(point.x, point.y);
-      ctx.lineTo(p.x, p.y);
-      ctx.lineWidth = 3;
-      ctx.strokeStyle = "rgba(" + p.red + "," + p.green + "," + p.blue + "," + point.active + ")";
-      ctx.stroke();
-    }
-  }
-}
-
-function drawCircle(circle) {
-  if (circle.active) {
-    ctx.beginPath();
-    ctx.arc(circle.position.x, circle.position.y, circle.radius, 0, 2 * Math.PI);
-    ctx.fillStyle = "rgba(" + circle.red + "," + circle.green + "," + circle.blue + "," + circle.active + ")";
-    ctx.fill();
-  }
-}
-
-function shiftPoint(point) {
-  point.red = Math.floor(Math.random() * 255);
-  point.green = Math.floor(Math.random() * 255);
-  point.blue = Math.floor(Math.random() * 255);
-  TweenLite.to(point, tweenDuration, {
-    x: point.originX - 50 + Math.random() * 100,
-    y: point.originY - 50 + Math.random() * 100,
-    ease: Sine.easeInOut,
-    onComplete: function() {
-      shiftPoint(point);
-    }
-  });
 }
 
 function mouseMoveHandler(e) {
-  target.x = e.clientX;
-  target.y = e.clientY;
+  mouse.x = e.clientX - canvas.offsetLeft;
+  mouse.y = e.clientY - canvas.offsetTop;
 }
 
 function resizeHandler() {
--- index.md
+++ index.md
@@ -1,3 +1,3 @@
 # Animation
 
-A beautiful sky animation with colorful and moving stars. Stars tween, change color and follow mouse. Written in ES6\. Made with Tween and EasePack. You can see it [here](https://berkerol.github.io/animation/animation.html).
+A beautiful sky animation with colorful and moving stars. Stars tween, follow mouse and change color with mouse click. Written in ES6\. Made with Tween and EasePack. You can see it [here](https://berkerol.github.io/animation/animation.html).
