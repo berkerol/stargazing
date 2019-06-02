9f62e4e12db98b96a00a99d17790f960026fb97f
Berk Erol <berk.erol@boun.edu.tr>
2019-06-02 16:10:11 +0300

    Combine semistandard rules

--- stargazing.js
+++ stargazing.js
@@ -1,6 +1,4 @@
-/* global Sine */
-/* global TweenLite */
-/* global FPSMeter */
+/* global $ Sine TweenLite FPSMeter */
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const background = document.getElementById('background');
